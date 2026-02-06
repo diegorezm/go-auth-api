38760a9a48c2ead771df29e33f8c9c6c97786fb9
diegorezm <dmourarezende@gmail.com>
2026-02-05 23:24:54 -0300

    Basic http handler config

---
+++ cmd/api/api.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP)    // import for rate limiting and analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+	// logger
+	db *pgx.Conn
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
